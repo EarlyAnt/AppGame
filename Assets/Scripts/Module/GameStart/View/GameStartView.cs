94bd27308d5c69483e5c3843ae14b82132194b3b
EarlyAnt <54763755@qq.com>
2021-01-22 17:43:25 +0800

    1.GameStartView.cs脚本，改为异步方式加载场景。

--- Assets/Scripts/Module/GameStart/View/GameStartView.cs
+++ Assets/Scripts/Module/GameStart/View/GameStartView.cs
@@ -23,7 +23,7 @@ namespace AppGame.Module.GameStart
         protected override void Start()
         {
             base.Start();
-            this.ReadConfig();
+            this.StartCoroutine(this.Initialize());
         }
 
         protected override void OnDestroy()
@@ -31,12 +31,41 @@ namespace AppGame.Module.GameStart
             base.OnDestroy();
         }
         /************************************************自 定 义 方 法************************************************/
-        private void ReadConfig()
+        //初始化
+        private IEnumerator Initialize()
         {
-            this.tweener = this.progressBar.DOFillAmount(1, Random.Range(this.durationMin, this.durationMax));
+            float progress = Random.Range(0.2f, 0.5f);
+            yield return this.StartCoroutine(this.ReadConfig(progress));
+            yield return this.StartCoroutine(this.LoadScene(progress, 1f));
+        }
+        //读取配置
+        private IEnumerator ReadConfig(float endValue)
+        {
+            bool complete = false;
+            this.tweener = this.progressBar.DOFillAmount(endValue, Random.Range(this.durationMin, this.durationMax));
             this.tweener.onUpdate = () => { this.progressValue.text = string.Format("{0:f1}%", this.progressBar.fillAmount * 100); };
-            this.tweener.onComplete = () => { SceneManager.LoadScene("TripMap", LoadSceneMode.Additive); };
+            this.tweener.onComplete = () => { complete = true; };
+            yield return new WaitUntil(() => complete == true);
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            Debug.Log("<><GameStartView.ReadConfig>Read config complete...");
+        }
+        //加载场景
+        private IEnumerator LoadScene(float startValue, float endValue)
+        {
+            AsyncOperation async = SceneManager.LoadSceneAsync("TripMap", LoadSceneMode.Additive);
+            async.allowSceneActivation = false;
+            while (async.progress < 0.9f)
+            {
+                this.progressBar.fillAmount = startValue + async.progress * (endValue - startValue);
+                this.progressValue.text = string.Format("{0:f1}%", startValue * 100 + async.progress * (endValue - startValue) * 100);
+                yield return new WaitForEndOfFrame();
+            }
+            Debug.Log("<><GameStartView.ReadConfig>Load scene async complete...");
+
+            this.progressBar.fillAmount = endValue;
+            this.progressValue.text = endValue * 100 + "%";
+            yield return new WaitForSeconds(Random.Range(1f, 2f));
+            async.allowSceneActivation = true;
         }
     }
 }
-
