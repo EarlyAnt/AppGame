0a4ce2b2e771c902dbbb82203c2dee261985c726
EarlyAnt <54763755@qq.com>
2021-01-27 13:10:06 +0800

    1.整理了地图事件状态控制机的代码。 2.ScenicNode.cs脚本，将InteractionNode脚本改造成景点介绍卡片的控制脚本。

--- Assets/Scripts/Module/Cycling/View/Component/ScenicNode.cs
+++ Assets/Scripts/Module/Cycling/View/Component/ScenicNode.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AppGame.Module.Cycling
 {
-    public class InteractionNode : MonoBehaviour
+    public class ScenicNode : BaseInteraction
     {
         /************************************************属性与变量命名************************************************/
         [SerializeField]
-        private string id;
+        private Image imageBox;
         [SerializeField]
-        private Interactions interaction;
-        public string ID { get { return this.id; } }
-        public Interactions Interacton { get { return this.interaction; } }
+        private Text textBox;
         /************************************************Unity方法与事件***********************************************/
 
         /************************************************自 定 义 方 法************************************************/
+        public override void Show()
+        {
+            //Todo: 从配置文件中读取要显示的图片，文字，及文字的国际化，显示卡片
+        }
+
+        public override void Hide()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
