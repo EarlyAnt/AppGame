514a3e43346c25fc3b4f04c4751d36ecee96eaab
EarlyAnt <54763755@qq.com>
2021-02-02 12:34:58 +0800

    1.经过测试发现，bg01和bg02这两张图片的显示效果和内存占用一样。(Unity中和手机上测试结果一样)

---
+++ Assets/Test/MapSize/MapSizeTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSizeTest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject map1;
+    [SerializeField]
+    private GameObject map2;
+
+    public void ShowMap1()
+    {
+        this.map1.SetActive(true);
+        this.map2.SetActive(false);
+    }
+
+    public void ShowMap2()
+    {
+        this.map1.SetActive(false);
+        this.map2.SetActive(true);
+    }
+}
