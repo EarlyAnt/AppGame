d27bdc2236634c2298633a9e6f7636f7f0fc958a
EarlyAnt <54763755@qq.com>
2021-02-23 15:30:25 +0800

    1.MpBall.cs脚本，增加了自动漂浮的动效。 2.Cycling.prefab预制体，调整MpBall组件上自动漂浮的参数。

--- Assets/Scripts/Module/Cycling/View/Component/MpBall.cs
+++ Assets/Scripts/Module/Cycling/View/Component/MpBall.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +7,27 @@ namespace AppGame.Module.Cycling
     public class MpBall : MonoBehaviour
     {
         /************************************************属性与变量命名************************************************/
+        #region 页面UI组件
         [SerializeField]
         private MpBallTypes mpBallType;
         [SerializeField]
         private Text mpBox;
         [SerializeField]
         private Text fromBox;
+        [SerializeField]
+        private CanvasGroup canvasGroup;
+        [SerializeField]
+        private Transform bubbleTransform;
+        [SerializeField, Range(0f, 100f)]
+        private float destinationY = 2f;
+        [SerializeField, Range(0.1f, 5f)]
+        private float durationMin = 2f;
+        [SerializeField, Range(0.1f, 5f)]
+        private float durationMax = 2f;
+        #endregion
+        #region 其他变量
+        private Tweener tweener;
+        private float originY;
         private int mp = 0;
         private string fromName = "";
         public int Value
@@ -51,12 +67,44 @@ namespace AppGame.Module.Cycling
             get { return this.fromName; }
             set { this.fromName = value; this.fromBox.text = value; }
         }
+        #endregion
         /************************************************Unity方法与事件***********************************************/
         private void Awake()
         {
             this.mpBox.text = "0";
         }
+        private void Start()
+        {
+            this.originY = this.bubbleTransform.localPosition.y;
+            this.destinationY += this.originY;
+            this.PlayPingPong();
+        }
         /************************************************自 定 义 方 法************************************************/
-
+        //设置是否可见
+        public void SetStatus(bool visible)
+        {
+            this.canvasGroup.DOFade(visible ? 1f : 0f, visible ? 0.2f : 0f);
+        }
+        //开始播放
+        public void PlayPingPong()
+        {
+            this.StopPingPong();
+            if (Random.Range(0, 10) % 2 == 0)
+                this.PingPong(this.originY, this.destinationY);
+            else
+                this.PingPong(this.originY, this.destinationY);
+        }
+        //停止播放
+        public void StopPingPong()
+        {
+            if (this.tweener != null)
+                this.tweener.Kill();
+        }
+        //往返运动
+        private void PingPong(float from, float to)
+        {
+            this.tweener = this.bubbleTransform.DOLocalMoveY(to, Random.Range(this.durationMin, this.durationMax));
+            this.tweener.onComplete += () => this.PingPong(to, from);
+        }
     }
 }
